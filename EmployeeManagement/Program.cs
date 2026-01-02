49d3319a8b6bab34b2a8bd672717cc382a31dbd3
Olukayode Adebiyi <olukayode.adebiyi@hotmail.co.uk>
2026-01-02 22:06:03 +0100

    Switch to SQL Server for database storage
    Replaced in-memory database with SQL Server in Program.cs and added the necessary EF Core SQL Server package. Updated connection string for local SQL Server instance. The in-memory configuration is now commented out.

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -2,6 +2,7 @@ using EmployeeManagement.Data;
 using EmployeeManagement.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.InMemory;
+
 namespace EmployeeManagement
 {
     public class Program
@@ -11,7 +12,17 @@ namespace EmployeeManagement
             var builder = WebApplication.CreateBuilder(args);
             // Add services to the container.
             builder.Services.AddDbContext<Data.AppDbContext>(options =>
-                options.UseInMemoryDatabase("EmployeeDb"));
+                options.UseSqlServer(
+                    "Data Source=OLUKAYODE\\SQLEXPRESS;" +
+                    "Database=Employees;" +
+                    "Integrated Security=True;" +
+                    "Persist Security Info=False;" +
+                    "Pooling=False;" +
+                    "MultipleActiveResultSets=False;" +
+                    "Encrypt=True;" +
+                    "TrustServerCertificate=True"
+                    ));
+            //options.UseInMemoryDatabase("EmployeeDb"));
 
             // Configure CORS to allow requests from any origin
             builder.Services.AddCors(options =>
